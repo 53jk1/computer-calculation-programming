9a31fc32d639753d9a2b34c6e94be211a20620c7
53jk1 <bonczalke@gmail.com>
2021-07-26 15:18:24 +0200

    fix: first task no whitespace

--- 01.py
+++ 01.py
@@ -3,6 +3,6 @@ x = np.array([1,3,4,3,2])
 y = np.array([1,1,2,5,4])
 
 def Area(x, y):
-return 0.5*np.abs(np.dot(x, np.roll(y,1))-np.dot(y,np.roll(x,1)))
+    return 0.5*np.abs(np.dot(x, np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
 print(Area(x, y))
