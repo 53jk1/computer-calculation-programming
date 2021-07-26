f78e69ffd6bb1c76c784cb7e3b45be69ebfdc80d
53jk1 <bonczalke@gmail.com>
2021-07-26 14:43:24 +0200

    fix: second task translation + bugfix

--- 02.py
+++ 02.py
@@ -1,7 +1,7 @@
 import numpy as np
-A = np.array([[1,2,3],[2,3,4],[3,4,5]])
+A = np.array([[1,2,3],[2,3,4],[3,4,1]])
 b = np.linalg.det(A)
 print(b)
 c = np.linalg.cond(A)
 print(c)
-print("Macierz jest dobrze uwarunkowana")
+print("The matrix is well coditioned.")
